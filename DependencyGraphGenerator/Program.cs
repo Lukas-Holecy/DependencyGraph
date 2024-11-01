2a27d5b9c3d94942174709ba9ca3f1a5f0690a7c
Lukas Holecy <lukas@brochet.co.uk>
2024-11-01 15:59:02 +0100

    Disabled top level statements

---
+++ DependencyGraphGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Holecy.Console.Dependencies;
+
+using System;
+
+/// <summary>
+/// Entry point of the application.
+/// </summary>
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
