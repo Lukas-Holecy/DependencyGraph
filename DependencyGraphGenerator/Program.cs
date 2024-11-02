e08209a7032b20cafdb6568f7e3f25256e15d59f
Lukas Holecy <lukasholecy@gmail.com>
2024-11-02 23:03:47 +0100

    The basic setup of CliFx is done
    Set up the main CliFx command.

--- DependencyGraphGenerator/Program.cs
+++ DependencyGraphGenerator/Program.cs
@@ -1,6 +1,9 @@
 ﻿namespace Holecy.Console.Dependencies;
 
+using System.IO.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
 using CliFx;
+
 
 /// <summary>
 /// Entry point of the application.
@@ -9,9 +12,23 @@ public class Program
 {
     public static async Task<int> Main(string[] args)
     {
+        var serviceProvider = SetUpDependencyInjection();
+
         return await new CliApplicationBuilder()
-            .AddCommandsFromThisAssembly()
+            .AddCommand<ProgramCommand>()
+            .UseTypeActivator(serviceProvider.GetService!)
             .Build()
             .RunAsync();
+    }
+
+    private static ServiceProvider SetUpDependencyInjection()
+    {
+        var services = new ServiceCollection()
+                    .AddSingleton<IFileSystem, FileSystem>()
+                    .AddTransient<ProgramCommand>();
+
+
+        var serviceProvider = services.BuildServiceProvider();
+        return serviceProvider;
     }
 }
